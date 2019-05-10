cee387ccecc37c2fe2a1d3ce9aeb958e024edeae
Gavin Rehkemper <grehkemper@esri.com>
2019-05-09 21:32:05 -0400

    router

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,45 @@
-import React from 'react';
-import { Greeter } from './Greeter';
-import PayeesGrid from './payees/PayeesGridFn';
-import { Payee } from './common/common-types';
-import PayeesManager from './payees/PayeesManagerFn';
-import PayeesFetcher from './payees/PayeesFetcher';
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Greeter } from "./Greeter";
+import PayeesGrid from "./payees/PayeesGridFn";
+import { Payee } from "./common/common-types";
+import PayeesManager from "./payees/PayeesManagerFn";
+import PayeesFetcher from "./payees/PayeesFetcher";
 
 const App: React.FC = () => {
   return (
-    <main className="container">
-      <header>
-        <Greeter companyName={'Weyland-Yutani Corporation'} />
-        <hr />
-      </header>
-      {/* <PayeesGrid payees={getPayees()} /> */}
-      <PayeesManager />
-      {/*       <PayeesFetcher>
-        {payees => {
-          if (payees.length > 0) {
-            return <PayeesGrid payees={payees} />;
-          } else {
-            return <p>Waiting....</p>;
-          }
-        }}
-      </PayeesFetcher>
- */}{' '}
-    </main>
+    <Router>
+      <main className="container">
+        <header>
+          <Greeter companyName={"Esri"} />
+          <hr />
+        </header>
+        <nav>
+          <ul className="list-inline">
+            <li className="list-inline-item">
+              <Link to="/">Home</Link>
+            </li>
+            <li className="list-inline-item">
+              <Link to="/payees-manager">Payees Manager</Link>
+            </li>
+          </ul>
+        </nav>
+        <Route path="/payees-manager" component={PayeesManager} />
+        <Route path="/" exact render={() => <h1>Welcome</h1>} />
+        {/* <PayeesGrid payees={getPayees()} /> */}
+        {/* <PayeesManager /> */}
+        {/*       <PayeesFetcher>
+          {payees => {
+            if (payees.length > 0) {
+              return <PayeesGrid payees={payees} />;
+            } else {
+              return <p>Waiting....</p>;
+            }
+          }}
+        </PayeesFetcher>
+  */}{" "}
+      </main>
+    </Router>
   );
 };
 
@@ -33,94 +48,94 @@ export default App;
 function getPayees(): Payee[] {
   return [
     {
-      id: '1',
+      id: "1",
       version: 1,
-      payeeName: 'DCH Mortgages',
+      payeeName: "DCH Mortgages",
       address: {
-        street: '1285 Rezlog Plaza',
-        city: 'Powhatan',
-        state: 'RI',
-        zip: '02212',
+        street: "1285 Rezlog Plaza",
+        city: "Powhatan",
+        state: "RI",
+        zip: "02212"
       },
-      categoryId: '102',
+      categoryId: "102",
       image: null,
       motto: null,
-      active: true,
+      active: true
     },
     {
-      id: '2',
+      id: "2",
       version: 1,
-      payeeName: 'Ill Communication Telephones',
+      payeeName: "Ill Communication Telephones",
       address: {
-        street: '1597 Figole Grove',
-        city: 'Akron',
-        state: 'OH',
-        zip: '66345',
+        street: "1597 Figole Grove",
+        city: "Akron",
+        state: "OH",
+        zip: "66345"
       },
-      categoryId: '106',
+      categoryId: "106",
       image: null,
       motto: null,
-      active: true,
+      active: true
     },
     {
-      id: '3',
+      id: "3",
       version: 1,
       payeeName: "Erol's Internet",
       address: {
-        street: '453 Loma Linda Junction',
-        city: 'Kansas City',
-        state: 'KS',
-        zip: '60019',
+        street: "453 Loma Linda Junction",
+        city: "Kansas City",
+        state: "KS",
+        zip: "60019"
       },
-      categoryId: '106',
+      categoryId: "106",
       image: null,
       motto: null,
-      active: true,
+      active: true
     },
     {
-      id: '4',
+      id: "4",
       version: 1,
-      payeeName: 'Acme Products, Inc',
+      payeeName: "Acme Products, Inc",
       address: {
-        street: '1669 Divided Court',
-        city: 'Cheyenne',
-        state: 'WY',
-        zip: '48324',
+        street: "1669 Divided Court",
+        city: "Cheyenne",
+        state: "WY",
+        zip: "48324"
       },
-      categoryId: '103',
+      categoryId: "103",
       image: null,
       motto: null,
-      active: true,
+      active: true
     },
     {
-      id: '5',
+      id: "5",
       version: 1,
-      payeeName: 'Shop-Rite Grocery Store',
+      payeeName: "Shop-Rite Grocery Store",
       address: {
-        street: '311 St. Paul Ave.',
-        city: 'Baltimore',
-        state: 'MD',
-        zip: '08697',
+        street: "311 St. Paul Ave.",
+        city: "Baltimore",
+        state: "MD",
+        zip: "08697"
       },
-      categoryId: '103',
+      categoryId: "103",
       image: null,
       motto: null,
-      active: true,
+      active: true
     },
     {
-      id: '6',
+      id: "6",
       version: 1,
-      payeeName: 'Sushi Land',
+      payeeName: "Sushi Land",
       address: {
-        street: '808 Calvert St.',
-        city: 'Baltimore',
-        state: 'MD',
-        zip: '01848',
+        street: "808 Calvert St.",
+        city: "Baltimore",
+        state: "MD",
+        zip: "01848"
       },
-      categoryId: '103',
+      categoryId: "103",
       image: null,
       motto: null,
-      active: true,
-    },
+      active: true
+    }
   ];
 }
